8bc173506a2d7f79e2341a92a732330eff2c729a
Sunil <sunilkrverma@gmail.com>
2015-08-23 22:31:55 -0400

    test

---
+++ cob/src/main/resources/static/controller.js
@@ -0,0 +1,8 @@
+var myApp = angular.module('myApp', []); // array is for dependencies
+
+/*myApp.controller('MyController', function MyController($scope, $http) {
+	$http.get('http://localhost:8080/listTask?processInstanceId=257535')
+			.success(function(data) {
+				$scope.greeting = data[0];
+			});
+});*/
